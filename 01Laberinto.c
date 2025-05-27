6a301cbf6eae376a8690d44dd64659a30f7b18b2
ItaliaHernandez <italiahero99@hotmail.com>
2025-05-26 23:40:26 -0600

    Implementacion de struct y visualizacion de interfaz para los tableros

--- 01Laberinto.c
+++ 01Laberinto.c
@@ -12,123 +12,157 @@
 #include <stdbool.h>
 #include <windows.h>
 #include <time.h>
+#include <string.h>
 
 //define limites del tablero
-#define FILAS 100 //->
-#define COLUMNAS 100
+#define FILAS 21 //->
+#define COLUMNAS 50
+
+//guarda las dimensiones del laberinto
+typedef struct {
+    int filas;
+    int columnas;
+} Dimensiones;
+
+//funcion que imprime el laberinto
+void mostrarLaberinto(char tablero[FILAS][COLUMNAS], int coordenadaX, int coordenadaY, Dimensiones dim) {
+    //system("cls");
+    for (int y = 0; y < dim.filas; y++) {
+        printf("\n");
+        for (int x = 0; x < dim.columnas; x++) {
+            //verifica si x & y es igual a las coordenadas de P
+            //si es imprime P sino lo que hay en esa casilla
+            if (x == coordenadaX && y == coordenadaY) {
+                printf("P");
+            } else {
+                printf("%c", tablero[y][x]);
+            }
+        }//fin for COLUMNAS
+    }//fin for FILAS
+}//fin funcion mortrarLaberinto
 
 //funcion que inicaliza el tablero
-inicializarTablero(char tablero[FILAS][COLUMNAS], int *coordenadaX, int *coordenadaY, int *salidaX, int *salidaY, int aleatorio){
-    char tableroInicial[FILAS][COLUMNAS];       //alamacena el tablero que el aleatorio indique
-	switch (aleatorio){
+void inicializarTablero(char tablero[FILAS][COLUMNAS], int *coordenadaX, int *coordenadaY, 
+                       int *salidaX, int *salidaY, int aleatorio, Dimensiones *dimensiones){
+    //limpia el tablero 
+    memset(tablero, '#', sizeof(char) * FILAS * COLUMNAS);
+    
+    //se elige el tablero segun el aleatorio
+    switch (aleatorio){
         case 0:
+            dimensiones->filas = 10;                    //guarda las dimensiones del tablero
+            dimensiones->columnas = 28;
             printf("\nTABLERO 1");
-            strcpy(tableroInicial[0],"############################");
-            strcpy(tableroInicial[1],"#P.........#...............#");
-            strcpy(tableroInicial[2],"##########.#.#############.#");
-            strcpy(tableroInicial[3],"#..........#.##.##.###.#####");
-            strcpy(tableroInicial[4],"#.########.................#");
-            strcpy(tableroInicial[5],"#........#.###############.#");
-            strcpy(tableroInicial[6],"#.######.#.#...............#");
-            strcpy(tableroInicial[7],"#.#......#.#.###############");
-            strcpy(tableroInicial[8],"#..........#..............X#");
-            strcpy(tableroInicial[9],"############################");
+            strcpy(tablero[0],"############################");
+            strcpy(tablero[1],"#P.........#...............#");
+            strcpy(tablero[2],"##########.#.#############.#");
+            strcpy(tablero[3],"#..........#.##.##.###.#####");
+            strcpy(tablero[4],"#.########.................#");
+            strcpy(tablero[5],"#........#.###############.#");
+            strcpy(tablero[6],"#.######.#.#...............#");
+            strcpy(tablero[7],"#.#......#.#.###############");
+            strcpy(tablero[8],"#..........#..............X#");
+            strcpy(tablero[9],"############################");
             break;
         case 1:
+            dimensiones->filas = 14;                    //guarda las dimensiones del tablero
+            dimensiones->columnas = 35;
             printf("\nTABLERO 2");
-            strcpy(tableroInicial[1],"#################################");
-            strcpy(tableroInicial[2],"#P..............................#");
-            strcpy(tableroInicial[3],"#.#####.#######################.#");
-            strcpy(tableroInicial[4],"#.#.....#.......#..........##...#");
-            strcpy(tableroInicial[5],"#.#.###.#.##.##.#.###.#######.#.#");
-            strcpy(tableroInicial[6],"#...#...#....##.#####.##......#.#");
-            strcpy(tableroInicial[7],"#####.###.#####.......#######.#.#");
-            strcpy(tableroInicial[8],"#.....###.#..########.#...###.#.#");
-            strcpy(tableroInicial[9],"#.####..#.##.......##.#.#.###.#.#");
-            strcpy(tableroInicial[10],"#.##...##.##.#####..#...#.....#.#");
-            strcpy(tableroInicial[11],"#.##.##.#.##.#...########.#####.#");
-            strcpy(tableroInicial[12],"#.......#X##.#.#.......##.#...#.#");
-            strcpy(tableroInicial[13],"#.#####..#......####...##...#...#");
-            strcpy(tableroInicial[14],"#################################");
+            strcpy(tablero[0],"#################################");
+            strcpy(tablero[1],"#P..............................#");
+            strcpy(tablero[2],"#.#####.#######################.#");
+            strcpy(tablero[3],"#.#.....#.......#..........##...#");
+            strcpy(tablero[4],"#.#.###.#.##.##.#.###.#######.#.#");
+            strcpy(tablero[5],"#...#...#....##.#####.##......#.#");
+            strcpy(tablero[6],"#####.###.#####.......#######.#.#");
+            strcpy(tablero[7],"#.....###.#..########.#...###.#.#");
+            strcpy(tablero[8],"#.####..#.##.......##.#.#.###.#.#");
+            strcpy(tablero[9],"#.##...##.##.#####..#...#.....#.#");
+            strcpy(tablero[10],"#.##.##.#.##.#...########.#####.#");
+            strcpy(tablero[11],"#.......#X##.#.#.......##.#...#.#");
+            strcpy(tablero[12],"#.#####..#......####...##...#...#");
+            strcpy(tablero[13],"#################################");
             break;
         case 2:
+            dimensiones->filas = 15;                    //guarda las dimensiones del tablero
+            dimensiones->columnas = 38;
             printf("\nTABLERO 3");
-            strcpy(tableroInicial[1],"######################################");
-            strcpy(tableroInicial[2],"#X..................####.##.####.....#");
-            strcpy(tableroInicial[3],"##.###.##.##.##.#.#.##...........###.#");
-            strcpy(tableroInicial[4],"##.##...........###.#...####.#####...#");
-            strcpy(tableroInicial[5],"##.#############..#.###.##.#....##.###");
-            strcpy(tableroInicial[6],"###..............##.#...##.####.##...#");
-            strcpy(tableroInicial[7],"#.#.#########.#####.#.####.#.##.##.#.#");
-            strcpy(tableroInicial[8],"#..................P.................#");
-            strcpy(tableroInicial[9],"#.##.###..#..##.###.##..##.###.##.##.#");
-            strcpy(tableroInicial[10],"#.####.......##.#.#.........#.....##.#");
-            strcpy(tableroInicial[11],"#.#####.##.#..#.#.#.#######.#####.##.#");
-            strcpy(tableroInicial[12],"#....#.###.####.#.##.....#...........#");
-            strcpy(tableroInicial[13],"####.#....#.....#.#.####.#######.#.###");
-            strcpy(tableroInicial[14],"#....####....##.....####.........#...#");
-            strcpy(tableroInicial[15],"######################################");
-            
+            strcpy(tablero[0],"######################################");
+            strcpy(tablero[1],"#X..................####.##.####.....#");
+            strcpy(tablero[2],"##.###.##.##.##.#.#.##...........###.#");
+            strcpy(tablero[3],"##.##...........###.#...####.#####...#");
+            strcpy(tablero[4],"##.#############..#.###.##.#....##.###");
+            strcpy(tablero[5],"###..............##.#...##.####.##...#");
+            strcpy(tablero[6],"#.#.#########.#####.#.####.#.##.##.#.#");
+            strcpy(tablero[7],"#..................P.................#");
+            strcpy(tablero[8],"#.##.###..#..##.###.##..##.###.##.##.#");
+            strcpy(tablero[9],"#.####.......##.#.#.........#.....##.#");
+            strcpy(tablero[10],"#.#####.##.#..#.#.#.#######.#####.##.#");
+            strcpy(tablero[11],"#....#.###.####.#.##.....#...........#");
+            strcpy(tablero[12],"####.#....#.....#.#.####.#######.#.###");
+            strcpy(tablero[13],"#....####....##.....####.........#...#");
+            strcpy(tablero[14],"######################################");           
             break;
         case 3:
+            dimensiones->filas = 21;                    //guarda las dimensiones del tablero
+            dimensiones->columnas = 48;
             printf("\nTABLERO 4");
-            strcpy(tableroInicial[0], "################################################");
-            strcpy(tableroInicial[1], "#P....#..#.....#.....#.....#.....#..#..........#");
-            strcpy(tableroInicial[2], "#.###.#.##.####.#.###.#.###.#.#.#...####.#####.#");
-            strcpy(tableroInicial[3], "#...#.......#...#...#.....#...#.#........#.....#");
-            strcpy(tableroInicial[4], "###.########.###.#.###.########.########.#.#####");
-            strcpy(tableroInicial[5], "#........#.......#...#......#....#.......#.....#");
-            strcpy(tableroInicial[6], "#.#######.#.#####.##########.#####.#.###.#.#.###");
-            strcpy(tableroInicial[7], "#.......#.#...#.#.....#..........#...#...#.#...#");
-            strcpy(tableroInicial[8], "#####.#.#####.#.###.#.##########.#####.###.#.###");
-            strcpy(tableroInicial[9], "##....#.#.#.....#...#.#.#..........#....#...#..#");
-            strcpy(tableroInicial[10],"###.#.#.#.#######.#.#.###########.#.#.#######.##");
-            strcpy(tableroInicial[11],"#...#.#...#.....#.#......#...#...#..#.#........#");
-            strcpy(tableroInicial[12],"#.#.#####.#.#.#.#####.##..#.#.#.#.#..#########.#");
-            strcpy(tableroInicial[13],"# #.....#.#.# #...#.....#.#...#.#.#.........#..#");
-            strcpy(tableroInicial[14],"###.#.#.###.###.#.#.#.###.#####.#####.#.#.#.#..#");
-            strcpy(tableroInicial[15],"#...#.#...#...#.#...#.#.......#.....#.#.#.#....#");
-            strcpy(tableroInicial[16],"###.#.#####.#####.###.#########.#.#.#.#.########");
-            strcpy(tableroInicial[17],"#...#.......#...#.#.........#.#.#.#.#...#......#");
-            strcpy(tableroInicial[18],"#.#######.#.#.#.#.#####.###.#.###########.##.#.#");
-            strcpy(tableroInicial[19],"#.......#.#.#...#.....#...#................#..X#");
-			strcpy(tableroInicial[20],"################################################");
+            strcpy(tablero[0], "################################################");
+            strcpy(tablero[1], "#P....#..#.....#.....#.....#.....#..#..........#");
+            strcpy(tablero[2], "#.###.#.##.####.#.###.#.###.#.#.#...####.#####.#");
+            strcpy(tablero[3], "#...#.......#...#...#.....#...#.#........#.....#");
+            strcpy(tablero[4], "###.########.###.#.###.########.########.#.#####");
+            strcpy(tablero[5], "#........#.......#...#......#....#.......#.....#");
+            strcpy(tablero[6], "#.#######.#.#####.##########.#####.#.###.#.#.###");
+            strcpy(tablero[7], "#.......#.#...#.#.....#..........#...#...#.#...#");
+            strcpy(tablero[8], "#####.#.#####.#.###.#.##########.#####.###.#.###");
+            strcpy(tablero[9], "##....#.#.#.....#...#.#.#..........#....#...#..#");
+            strcpy(tablero[10],"###.#.#.#.#######.#.#.###########.#.#.#######.##");
+            strcpy(tablero[11],"#...#.#...#.....#.#......#...#...#..#.#........#");
+            strcpy(tablero[12],"#.#.#####.#.#.#.#####.##..#.#.#.#.#..#########.#");
+            strcpy(tablero[13],"# #.....#.#.# #...#.....#.#...#.#.#.........#..#");
+            strcpy(tablero[14],"###.#.#.###.###.#.#.#.###.#####.#####.#.#.#.#..#");
+            strcpy(tablero[15],"#...#.#...#...#.#...#.#.......#.....#.#.#.#....#");
+            strcpy(tablero[16],"###.#.#####.#####.###.#########.#.#.#.#.########");
+            strcpy(tablero[17],"#...#.......#...#.#.........#.#.#.#.#...#......#");
+            strcpy(tablero[18],"#.#######.#.#.#.#.#####.###.#.###########.##.#.#");
+            strcpy(tablero[19],"#.......#.#.#...#.....#...#................#..X#");
+			strcpy(tablero[20],"################################################");
             break;
     }//fin switch
     
     //recorre todo el mapa 
-    for (int y = 0; y < FILAS; y++) {
-        for (int x = 0; x < COLUMNAS; x++) {
-            //asigna lo que hay en esa posicion a la matriz del parametro
-            tablero[y][x] = tableroInicial[y][x];
+    for (int y = 0; y < dimensiones->filas; y++) {
+        for (int x = 0; x < dimensiones->columnas; x++) {
             if (tablero[y][x] == 'P') {
                 //guarda las coordenadas de P
                 *coordenadaX = x;
                 *coordenadaY = y;
-            }
-            else if (tablero[y][x] == 'X') {
+                //cuando P se mueve deja un punto en su lugar 
+                tablero[y][x] = '.';
+            }else if (tablero[y][x] == 'X') {
                 //guarda las coordenadas de X
                 *salidaX = x;
                 *salidaY = y;
-            }
+            }//fin if-else
         }//fin for COLUMNAS
     }//fin for FILAS
-    
 }//fin funcion inicializarTablero 
 
-
-
-
 int main() {
+    srand(time(NULL));
     char tablero[FILAS][COLUMNAS];                    //almacena matriz del tablero
     int coordenadaX, coordenadaY, salidaX, salidaY;   //coordenas de P y X
     char tecla;                                       //almacena que tecla es la que se presiona
+    Dimensiones dimensionesActuales;                  //almacena la dimensiones del tablero
 
     do{
-      	int aleatorio = 0 + rand() % (5 - 0 + 1);    //aleatorio para escoger tablero
+      	int aleatorio = 0 + rand() % (5 - 0 + 1);     //aleatorio para escoger tablero
 
-        inicializarTablero(tablero, &coordenadaX, &coordenadaY, &salidaX, &salidaY, aleatorio);
-
+        inicializarTablero(tablero, &coordenadaX, &coordenadaY, &salidaX, &salidaY, aleatorio, &dimensionesActuales);
+        
+        mostrarLaberinto(tablero, coordenadaX, coordenadaY, dimensionesActuales);
+            
 
 
 
